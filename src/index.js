9587469880e896c3b1faabcf50c807c02cd335e3
Angel Torres <avto9711@gmail.com>
2020-03-27 00:48:07 -0400

    best format

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const express =  require("express");
-
 const app  = express();
 const routesV1 =  require('./routes/v1');
-
-// db.sequelize()
 routesV1(app);
 app.listen(3000,()=>{
     console.log("Running on 3000")
