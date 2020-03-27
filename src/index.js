a482350e769fea2490f3f6a78260b3f3210c39bf
Angel Torres <avto9711@gmail.com>
2020-03-27 01:10:08 -0400

    Fix env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+const env = require('dotenv').config()
 const express =  require("express");
 const app  = express();
 const routesV1 =  require('./routes/v1');
+
+console.log(env)
+
 routesV1(app);
 app.listen(3000,()=>{
     console.log("Running on 3000")
--- src/squalize/index.js
+++ src/squalize/index.js
@@ -1,8 +1,7 @@
 const Sequelize = require('sequelize');
 
-// Option 1: Passing parameters separately
-const sequelize = new Sequelize('LACEmployeeStaff', 'sxdefault', 's1uEQ3IAZz7Ns3sG', {
-  host: 'solvedevdb.database.windows.net',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect:  'mssql',
   port:1433,
   dialectOptions: {
